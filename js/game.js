0ddacfec47693c394ab575f29791a5c31bba1dbb
Erik Song <es2fq@virginia.edu>
2017-10-20 23:30:27 -0400

    merged

--- js/game.js
+++ js/game.js
@@ -5,28 +5,22 @@ var roomWidth = 1000;
 var WIDTH = 7 * roomWidth;
 var HEIGHT = 500;
 
+// var background;
 
-function homeload() {
-    game.load.spritesheet('water', 'assets/water.png', 32, 400, 32)
-}
+// background = game.add.tileSprite(0, 24*16, 128*16, 24*16, 'waters');
 
-var background
+// background.animations.add('waves0', [0, 1, 2, 3, 2, 1]);
+// background.animations.add('waves1', [4, 5, 6, 7, 6, 5]);
+// background.animations.add('waves2', [8, 9, 10, 11, 10, 9]);
+// background.animations.add('waves3', [12, 13, 14, 15, 14, 13]);
+// background.animations.add('waves4', [16, 17, 18, 19, 18, 17]);
+// background.animations.add('waves5', [20, 21, 22, 23, 22, 21]);
+// background.animations.add('waves6', [24, 25, 26, 27, 26, 25]);
+// background.animations.add('waves7', [28, 29, 30, 31, 30, 29]);
 
-background = game.add.tileSprite(0, 24*16, 128*16, 24*16, 'waters')
-
-background.animations.add('waves0', [0, 1, 2, 3, 2, 1]);
-    background.animations.add('waves1', [4, 5, 6, 7, 6, 5]);
-    background.animations.add('waves2', [8, 9, 10, 11, 10, 9]);
-    background.animations.add('waves3', [12, 13, 14, 15, 14, 13]);
-    background.animations.add('waves4', [16, 17, 18, 19, 18, 17]);
-    background.animations.add('waves5', [20, 21, 22, 23, 22, 21]);
-    background.animations.add('waves6', [24, 25, 26, 27, 26, 25]);
-    background.animations.add('waves7', [28, 29, 30, 31, 30, 29]);
-
-var n = 7;
-background.animations.play('waves' + n, 8, true);
-}
-/*When the start button is clicked the game begins...*/
+// var n = 7;
+// background.animations.play('waves' + n, 8, true);
+// //[>When the start button is clicked the game begins...<]
 
 
 var player;
@@ -115,6 +109,7 @@ function preload() {
     game.load.image('fish', 'assets/fish.png');
     game.load.image('paper', 'assets/paper.png');
     game.load.image('spongebob', 'assets/spongebob.png');
+    game.load.spritesheet('water', 'assets/water.png', 32, 400, 32)
 }
 
 function create() {
